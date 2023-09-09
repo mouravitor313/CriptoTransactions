9a3a8adf666e88f90122ebd8724d8a1338477787
Vítor Moura <123372633+mouravitor313@users.noreply.github.com>
2023-09-09 10:54:50 -0300

    update main
    Main file improved with a better navigation for the user and now with verification of whether the transfer value is greater than the wallet value.

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
 from check_balance import Wallet
 from transaction import Transaction
+from time import sleep
+from os import system
+
 
 class Main:
     def __init__(self) -> None:
         pass
 
-# Criação de uma carteira
-wallet = Wallet.get_id()
-print(wallet.verification())
-print(wallet.balance())
-print(wallet.id)
+    def main(self):
+        while True:
+            wallet = Wallet.get_id()
+            system('cls')
+            balance = wallet.balance()
+            print(f'Your current balance is: {balance} ETH')
+            choice = input('Do you want to make a transaction? [Y/N]').upper()
+            if choice == 'Y':
+                private = Transaction.get_private()
+                estrangeira = Transaction.get_estrangeira()
+                value = Transaction.get_value()
+                if value > balance:
+                    print('Error: Insufficient funds in wallet for transaction.')
+                    sleep(2)
+                else:
+                    transaction = Transaction(private, estrangeira, value)
+                    transaction.transaction()
+                    sleep(2)
+            else:
+                break
 
-# Criação de uma transação
-private = Transaction.get_private()
-estrangeira = Transaction.get_estrangeira()
-value = Transaction.get_value()
-transaction = Transaction(private, estrangeira, value)
-transaction.transaction()
+
+
+# verifications:
+# print(wallet.verification())
+# print(wallet.id)
+
+
+
+
+
+
